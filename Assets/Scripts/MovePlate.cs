a1f641e16999ba3b50d72053f8dde22355847269
Md. Fatin Shadab Turja <71595077+FatinShadab@users.noreply.github.com>
2025-05-31 09:17:41 +0600

    Update MovePlate.cs

--- Assets/Scripts/MovePlate.cs
+++ Assets/Scripts/MovePlate.cs
@@ -4,73 +4,87 @@ using UnityEngine;
 
 public class MovePlate : MonoBehaviour
 {
-    //Some functions will need reference to the controller
+    // A reference to the GameController (needed to access and update game state)
     public GameObject controller;
 
-    //The Chesspiece that was tapped to create this MovePlate
+    // The Chessman (piece) that this move plate belongs to (i.e., the one the player selected)
     GameObject reference = null;
 
-    //Location on the board
+    // The target X and Y position this move plate represents on the chessboard
     int matrixX;
     int matrixY;
 
-    //false: movement, true: attacking
+    // If true, this move plate indicates an attack move (not just a normal move)
     public bool attack = false;
 
+    // Called when this MovePlate object is created in the scene
     public void Start()
     {
+        // If it's an attack plate, change its color to red for visual feedback
         if (attack)
         {
-            //Set to red
-            gameObject.GetComponent<SpriteRenderer>().color = new Color(255.0f, 255.0f, 255.0f, 1.0f);
+            // Note: This uses a SpriteRenderer, so it's for 2D games. Use material.color for 3D.
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255.0f, 0.0f, 0.0f, 1.0f);
         }
     }
 
+    // Called when the player clicks on this MovePlate
     void OnMouseUp()
     {
+        // Find and store reference to the GameController
         controller = GameObject.FindGameObjectWithTag("GameController");
 
-        //Destroy the victim Chesspiece
+        // If this move is an attack move, destroy the chess piece currently at this location
         if (attack)
         {
+            // Get the chess piece at the target location
             GameObject cp = controller.GetComponent<Game>().GetPosition(matrixX, matrixY);
 
+            // If the captured piece is a King, the game ends and the opponent wins
             if (cp.name == "wKing") controller.GetComponent<Game>().handleChessWinner("black");
             if (cp.name == "bKing") controller.GetComponent<Game>().handleChessWinner("white");
 
+            // Remove the enemy piece from the scene
             Destroy(cp);
         }
 
-        //Set the Chesspiece's original location to be empty
-        controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(), 
-            reference.GetComponent<Chessman>().GetYBoard());
+        // Clear the piece's previous position from the game board logic
+        controller.GetComponent<Game>().SetPositionEmpty(
+            reference.GetComponent<Chessman>().GetXBoard(),
+            reference.GetComponent<Chessman>().GetYBoard()
+        );
 
-        //Move reference chess piece to this position
+        // Update the piece's logical board coordinates to the new position
         reference.GetComponent<Chessman>().SetXBoard(matrixX);
         reference.GetComponent<Chessman>().SetYBoard(matrixY);
+
+        // Move the piece's GameObject in the scene to the new location
         reference.GetComponent<Chessman>().SetCoords();
 
-        //Update the matrix
+        // Update the game board matrix to reflect the new position
         controller.GetComponent<Game>().SetPosition(reference);
 
-        //Switch Current Player
+        // Change the turn to the next player
         controller.GetComponent<Game>().NextTurn();
 
-        //Destroy the move plates including self
+        // Remove all move plates from the board (including this one)
         reference.GetComponent<Chessman>().DestroyMovePlates();
     }
 
+    // Set the board coordinates this move plate represents
     public void SetCoords(int x, int y)
     {
         matrixX = x;
         matrixY = y;
     }
 
+    // Set which piece this move plate is associated with
     public void SetReference(GameObject obj)
     {
         reference = obj;
     }
 
+    // Get the reference to the chess piece that owns this move plate
     public GameObject GetReference()
     {
         return reference;
